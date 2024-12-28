c4ceafd07b850cd1e8c714ec73e648d066cc8d81
kartik417 <kartiksharma242001@gmail.com>
2024-12-28 19:14:43 +0530

    Delete Fragment directory

--- Fragment/app/src/main/java/com/example/fragment/Clock.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fragment
-
-import androidx.fragment.app.Fragment
-
-class Clock: Fragment(R.layout.fragment_clock) {
-}
--- Fragment/app/src/main/java/com/example/fragment/DateSheet.kt
+++
@@ -1,7 +0,0 @@
-package com.example.fragment
-
-import androidx.fragment.app.Fragment
-
-class DateSheet: Fragment(R.layout.exam_date_sheet) {
-
-}
